372149c272c09c5ad32b82170c4fddfe1946bb5f
Dawn Schumacher <dawn@berkeley.edu>
2025-02-23 19:37:38 -0800

    Last autoreload fix pls

--- server/routing/infra.py
+++ server/routing/infra.py
@@ -4,7 +4,7 @@ import subprocess
 
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
-import server.loading.cache as cache
+#import server.loading.cache as cache
 
 router = APIRouter()
 
@@ -12,7 +12,7 @@ def verify_signature(payload_body, secret, signature_header):
     if not signature_header:
         return False
 
-    hash_object = hmac.new(secret.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+    hash_object = hmac.new(secret.encode('utf-8'), msg=payload_body.encode('utf-8'), digestmod=hashlib.sha256)
     expected_signature = "sha256=" + hash_object.hexdigest()
 
     if not hmac.compare_digest(expected_signature, signature_header):
