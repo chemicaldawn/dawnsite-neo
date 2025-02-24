44fd5f6fd252dfcb062c8af8d25659c9d41492db
Dawn Schumacher <dawn@berkeley.edu>
2025-02-23 19:27:19 -0800

    Fix autoreload hook

--- server/routing/infra.py
+++ server/routing/infra.py
@@ -12,7 +12,7 @@ def verify_signature(payload_body, secret, signature_header):
     if not signature_header:
         return False
 
-    hash_object = hmac.new(secret_token.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
+    hash_object = hmac.new(secret.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
     expected_signature = "sha256=" + hash_object.hexdigest()
 
     if not hmac.compare_digest(expected_signature, signature_header):
