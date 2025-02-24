f7e95c3d87212c4b5146a1795c61cebb6c3398fa
Dawn Schumacher <dawn@berkeley.edu>
2025-02-23 19:39:50 -0800

    Last autoreload fix pls

--- server/routing/infra.py
+++ server/routing/infra.py
@@ -12,7 +12,7 @@ def verify_signature(payload_body, secret, signature_header):
     if not signature_header:
         return False
 
-    hash_object = hmac.new(secret.encode('utf-8'), msg=payload_body.encode('utf-8'), digestmod=hashlib.sha256)
+    hash_object = hmac.new(secret.encode('utf-8'), msg=payload_body, digestmod=hashlib.sha256)
     expected_signature = "sha256=" + hash_object.hexdigest()
 
     if not hmac.compare_digest(expected_signature, signature_header):
@@ -22,6 +22,6 @@ def verify_signature(payload_body, secret, signature_header):
 
 @router.post("/git/onpush", response_class = HTMLResponse)
 def onpush(request : Request):
-    if (verify_signature(request.body, cache.secrets["github"], request.headers.get("X-Hub-Signature-256"))):
+    if (verify_signature(request.body(), cache.secrets["github"], request.headers.get("X-Hub-Signature-256"))):
         subprocess.run(["sh","./reload.sh"])
     print("Reloading server...")
